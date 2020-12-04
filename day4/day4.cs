ddbb61c608acccc08f5f3a5a90038de0d5f1e6f6
Fnord <FnordGitHub@outlook.de>
2020-12-04 11:17:46 +0100

    part2 done, validation not fully working yet

--- day4/day4.cs
+++ day4/day4.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System;
+using System.Text.RegularExpressions;
 
 namespace adventOfCode2020 {
 
@@ -42,11 +43,114 @@ namespace adventOfCode2020 {
 
         private bool validateFields(passport input) {
 
+            bool isValid = false;
+            List<string> validEyeColors = new List<string>(new string[] {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"});
+            string hgtMatch = "\\d+(in|cm)";
+            string hclMatch = "\\#[a-z0-9]{6}";
+            string pidMatch = "\\d{9}";
+
             foreach (KeyValuePair<string, string> field in input.getFields()) {
 
-                
+                switch (field.Key) {
+
+                    case "byr":
+                        if (long.Parse(field.Value) >= 1920 && long.Parse(field.Value) <=2002) {
+                            isValid = true;
+                        }
+                        else {
+                            isValid = false;
+                        }
+                        break;
+
+                    case "iyr":
+                        if (long.Parse(field.Value) >= 2010 && long.Parse(field.Value) <=2020) {
+                            isValid = true;
+                        }
+                        else {
+                            isValid = false;
+                        }
+                        break;
+
+                    case "eyr":
+                        if (long.Parse(field.Value) >= 2020 && long.Parse(field.Value) <=2030) {
+                            isValid = true;
+                        }
+                        else {
+                            isValid = false;
+                        }
+                        break;
+
+                    case "hgt":
+                        if (Regex.IsMatch(field.Value, hgtMatch)) {
+                            string unit = field.Value.Substring(field.Value.Length - 2, 2);
+                            long value = long.Parse(field.Value.Substring(0, field.Value.Length -2));
+                            switch (unit) {
+
+                                case "cm":
+                                    if (value >= 150 && value <= 192) {
+                                        isValid = true;
+                                    }
+                                    else {
+                                        isValid = false;
+                                    }
+                                    break;
+                                
+                                case "in":
+                                    if (value >= 59 && value <= 76) {
+                                        isValid = true;
+                                    }
+                                    else {
+                                        isValid = false;
+                                    }
+                                    break;
+
+                                default:
+                                    isValid = false;
+                                    break;
+
+                            }
+                        }
+                        else {
+                            isValid = false;
+                        }
+                        break;
+
+                    case "hcl":
+                        if (Regex.IsMatch(field.Value, hclMatch)) {
+                            isValid = true;
+                        }
+                        else {
+                            isValid = false;
+                        }
+                        break;
+
+                    case "ecl":
+                        if (validEyeColors.Contains(field.Value)) {
+                            isValid = true;
+                        }
+                        else {
+                            isValid = false;
+                        }
+                        break;
+
+                    case "pid":
+                        if (Regex.IsMatch(field.Value, pidMatch)) {
+                            isValid = true;
+                        }
+                        else {
+                            isValid = false;
+                        }
+                        break;
+
+
+                    default:
+                        break;
+
+                }
 
             }
+
+            return isValid;
 
         }       
 
@@ -66,9 +170,19 @@ namespace adventOfCode2020 {
 
             foreach (passport pass in input) {
 
-                bool passValid = validateFields(pass);
+                bool allPresent = true;
 
-                if (passValid) {
+                foreach (string field in necessaryFields) {
+
+                    if (!pass.getFields().ContainsKey(field)) {
+
+                        allPresent = false;
+
+                    }
+
+                }
+
+                if (allPresent) {
 
                     validPasses.Add(pass);
 
@@ -86,15 +200,27 @@ namespace adventOfCode2020 {
 
             foreach (passport pass in input) {
 
+                bool allPresent = true;
                 bool passValid = true;
 
                 foreach (string field in necessaryFields) {
 
                     if (!pass.getFields().ContainsKey(field)) {
 
-                        passValid = false;
+                        allPresent = false;
 
                     }
+
+                }
+
+                if (allPresent) {
+
+                    passValid = validateFields(pass);
+
+                }
+                else {
+
+                     passValid = false;
 
                 }
 
@@ -124,15 +250,15 @@ namespace adventOfCode2020 {
             string[] necessaryFields = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"};
 
             TimeSpan timer1 = timer.measuredTime(() => partOne(passportData,new List<string>(necessaryFields)));
-            //TimeSpan timer2 = timer.measuredTime(() => partTwo(passportData,new List<string>(necessaryFields), new List<string>(optionalFields)));
+            TimeSpan timer2 = timer.measuredTime(() => partTwo(passportData,new List<string>(necessaryFields)));
 
             long result = partOne(passportData,new List<string>(necessaryFields));
-            //long resultPartTwo = partTwo(passportData,new List<string>(necessaryFields), new List<string>(optionalFields));
+            long resultPartTwo = partTwo(passportData,new List<string>(necessaryFields));
 
             write.timesTaken.Add(timer1);
-            //write.timesTaken.Add(timer2);
+            write.timesTaken.Add(timer2);
             write.partResults.Add("part one", result.ToString());
-            //write.partResults.Add("Part two", resultPartTwo.ToString());
+            write.partResults.Add("Part two", resultPartTwo.ToString());
 
             write.writeResults(day);
 
