2c85db5c2b38489eedc4d2f2fb53701430e93389
Fnord <FnordGitHub@outlook.de>
2020-12-09 23:50:18 +0100

    prepared day 10

---
+++ day10/day10.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+
+namespace adventOfCode2020 {
+
+    class day10 : dayBase {
+
+        public override long partOne(List<string> input)
+        {
+
+            long result = 0;
+
+
+
+            return result;
+
+        }
+
+        public override long partTwo(List<string> input)
+        {
+
+            long result = 0;
+
+
+
+            return result;
+
+        }
+
+    }
+
+}
